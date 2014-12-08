78b3f55927d45069f660cf22f198298fc73c6d35
Marius Gundersen <me@mariusgundersen.net>
2014-12-09 00:36:49 +0100

    Error handling in a SPA

---
+++ www_source/examples/advent/09/main.js
@@ -0,0 +1,11 @@
+require.config({
+  paths:{
+    'knockout': "/bower_components/knockout/dist/knockout"
+  },
+  packages:[
+    {name:'deco', location:'/bower_components/deco/Dist', main:'deco'}
+  ]
+});
+require(["deco"], function(deco){
+  deco.config({}).start();
+});
