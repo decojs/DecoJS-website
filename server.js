0c43a0476da386b0dadbcd76ff74a9fd5dbb6594
Marius Gundersen <Marius.Gundersen@ciber.com>
2015-01-24 02:01:05 +0100

    redirect snapshots to the right place

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 var express = require("express");
 
-express().use(express.static(__dirname+"/www")).listen(8080);
+var app = express();
+
+app.use(function(req, res, next){
+  if('_escaped_fragment_' in req.query){
+    var match = /^(\/??)(.*?)(\/?)$/.exec(req.query['_escaped_fragment_']);
+    if(match){
+      res.redirect([
+        '/snapshot/',
+        match[2],
+        (match[3] ? '/index' : ''),
+        '.html'
+      ].join(''));
+      return;
+    }
+  }
+  
+  next();
+})
+
+app.use(express.static(__dirname+"/www"));
+
+app.listen(8080);
