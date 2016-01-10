cc238aad3dd15021f264ce0196757d59bd9b8f4e
Marius Gundersen <me@mariusgundersen.net>
2016-01-10 21:43:50 +0100

    Fixed fragment snapshotting

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ var app = express();
 
 app.use(function(req, res, next){
   if('_escaped_fragment_' in req.query){
-    var match = /^(\/??)(.*?)(\/?)$/.exec(req.query['_escaped_fragment_']);
-    if(match){
-      res.redirect([
-        '/snapshot/',
-        match[2],
-        (match[3] ? '/index' : ''),
-        '.html'
-      ].join(''));
-      return;
-    }
+    console.log('_escapted_fragment');
+    var path = req.query['_escaped_fragment_'];
+    console.log(path);
+    if(path[0] == '/') path = path.substr(1);
+    if(path[path.length-1] == '/') path += 'index';
+    path += '.html';
+    req.url = '/snapshot/'+path;
+    console.log(req.url);
   }
-  
+
   next();
 })
 
